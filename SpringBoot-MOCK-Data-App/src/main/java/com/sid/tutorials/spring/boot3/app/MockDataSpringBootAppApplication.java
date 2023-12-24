e7c3b3db1888d1d9974492103fa8863b2d3603bf
javawithinfinitepossibilities@gmail.com <javawithinfinitepossibilities@gmail.com>
2023-12-24 14:28:24 +0530

    Added the MOCK data module

---
+++ SpringBoot-MOCK-Data-App/src/main/java/com/sid/tutorials/spring/boot3/app/MockDataSpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.sid.tutorials.spring.boot3.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kunmu On 24-12-2023
+ */
+@SpringBootApplication
+public class MockDataSpringBootAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MockDataSpringBootAppApplication.class,args);
+    }
+}
---
+++ SpringBoot-MOCK-Data-App/src/test/java/com/sid/tutorials/spring/boot3/app/MockDataSpringBootAppApplicationTest.java
@@ -0,0 +1,12 @@
+package com.sid.tutorials.spring.boot3.app;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author kunmu On 24-12-2023
+ */
+@SpringBootTest(classes = MockDataSpringBootAppApplication.class)
+public class MockDataSpringBootAppApplicationTest {
+}
